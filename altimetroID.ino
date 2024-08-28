f673c67bf4defd9cb2f29b0f93bc985b65da5899
alecampo01 <167711900+alecampo01@users.noreply.github.com>
2024-08-27 22:46:58 -0400

    Add files via upload

--- altimetroID.ino
+++ altimetroID.ino
@@ -1,54 +1,47 @@
 #include <sensor.h>
 #include <bmp280.h>
 
-#define minTemp 20
-#define maxTemp 300
-#define minPr 0
-#define maxPr 500
-#define minAlt 0
-#define maxAlt 1100
-
 BMP280 bmpSensor;
 
 float TEMPERATURA;  
 float PRESION;     
 float ALTITUD;
 
+unsigned long previousMillis=0;
+const long interval=5000;
+
 void setup() {
-  Serial.begin(9600);       
+  Serial.begin(9600);     
   Serial.println("Iniciando: ");  
   if (!bmpSensor.begin()) {       
-    Serial.println("BMP280 no encontrado !");  
+    Serial.println("BMP280 no encontrado!");  
     while (1);            
   }
 }
 
 void loop() {
+
+unsigned long currentMillis = millis();
+
+if (currentMillis - previousMillis >= interval) {
+  previousMillis = currentMillis;
+
   TEMPERATURA = bmpSensor.readTemperature(); 
-   if(TEMPERATURA<=maxTemp && TEMPERATURA>=minTemp){
-    Serial.print("Temperatura: ");
-    Serial.print(TEMPERATURA);
-    Serial.println(" C");
-  }else{
-    Serial.println("Temperatura no válida");
-  }
-
   PRESION = bmpSensor.readPressure()/100; 
-  if(PRESION<=maxPr && PRESION>=minTemp){
-    Serial.print("Presion: ");
-    Serial.println(" hPa");
-  }else{
-    Serial.println("Presión no válida");
-  }
-
   ALTITUD = bmpSensor.readAltitude();
-  if(ALTITUD<=maxAlt && ALTITUD>=minAlt){
-    Serial.print("Altitud: ");
-    Serial.println(" m");
-  }else{
-    Serial.println("Altitud no válida");
-  }
 
+  Serial.print("Temperatura: "); 
+  Serial.print(TEMPERATURA);  
+  Serial.print(" C ");  
 
-  delay(5000); // demora de 5 segundos entre lecturas
+  Serial.print("Presion: ");  
+  Serial.print(PRESION);  
+  Serial.println(" hPa");  
+
+  Serial.print("Altitud: ");  
+  Serial.print(ALTITUD);  
+  Serial.println(" m");  
+
+}
+  
 }
